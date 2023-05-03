f6daf7ccfa94b5a2801ce770b52777bb71cda4e8
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-05-03 15:54:29 +0000

    修正

--- components/layout.js
+++ components/layout.js
@@ -32,7 +32,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="${router.basePath}/images/profile.jpg"
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -45,7 +45,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="${router.basePath}/images/profile.jpg"
+                src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
