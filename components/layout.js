0c0dea8a6c0fb702d503c93715874c9a7548f328
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-05-05 03:05:10 +0000

    サイト名変更

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'AIデザインショーケース';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'AIデザインショーケース';
 
 export default function Layout({ children, home }) {
   return (
