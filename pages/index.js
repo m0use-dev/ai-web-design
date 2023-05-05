a6de6abef5fc3cc83480258fe32d9a5971d2e54c
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-05-05 17:16:47 +0000

    修正

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-// import Date from '../components/date';
-// import Tags from '../components/tags';
+import Date from '../components/date';
+import Tags from '../components/tags';
 import Thumbnail from '../components/thumbnail';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
