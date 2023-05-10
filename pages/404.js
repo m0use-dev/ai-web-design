242b35ea78f7dac7f2e135470c3a21f57e894bbf
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-05-10 15:43:07 +0000

    404作業

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,7 @@
+import Layout, { siteTitle } from "../components/layout";
+import main from "../components/css/main.module.css";
+import common from "../components/css/common.module.css";
+
 export default function Custom404() {
   return <h1>404 - Page Not Found</h1>;
 }
